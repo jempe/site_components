846230c277e1173efe6d20f7455bfa5af0240bad
Kastro <info@jempe.org>
2025-11-16 17:24:17 -0500

    move to a slide when stopped in the middle

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,56 +1,80 @@
 class JempeSlideshow {
-    constructor(elementID, options) {
-        this.element = document.getElementById(elementID);
-        if (!this.element) {
-            console.error(`Element with ID "${elementID}" not found.`);
-            return;
-        }
+	constructor(elementID, options) {
+		this.element = document.getElementById(elementID);
+		if (!this.element) {
+			console.error(`Element with ID "${elementID}" not found.`);
+			return;
+		}
 
-        const defaultOptions = {
-            slideWidth: "200px",
-            slideGap: "20px",
-        };
+		const defaultOptions = {
+			slideWidth: "200px",
+			slideGap: "20px",
+		};
 
-        this.options = { ...defaultOptions, ...options };
+		this.options = { ...defaultOptions, ...options };
 
-        this.init();
-    }
+		this.init();
+	}
 
-    init() {
-        this.slidesContainer = this.element.querySelector('.jempe_slideshow');
-        this.slides = this.element.querySelectorAll('.jempe_slideshow_item');
-        this.currentIndex = 0;
-        this.update();
-    }
+	init() {
+		this.slidesContainer = this.element.querySelector(".jempe_slideshow");
+		this.slides = this.element.querySelectorAll(".jempe_slideshow_item");
+		this.currentIndex = 0;
+		this.isProgrammaticScroll = false;
 
-    next() {
-        if (this.currentIndex < this.slides.length - 1) {
-            this.currentIndex++;
-            this.update();
-        }
-    }
+		this.slidesContainer.addEventListener("scrollend", () => {
+			if (this.isProgrammaticScroll) {
+				return;
+			}
+			this.setIndexAfterScroll();
+		});
 
-    prev() {
-        if (this.currentIndex > 0) {
-            this.currentIndex--;
-            this.update();
-        }
-    }
+		this.update();
+	}
 
-    goTo(index) {
-        if (index >= 0 && index < this.slides.length) {
-            this.currentIndex = index;
-            this.update();
-        }
-    }
+	next() {
+		if (this.currentIndex < this.slides.length - 1) {
+			this.currentIndex++;
+			this.update();
+		}
+	}
 
-    update() {
-        const slideWidth = parseInt(this.options.slideWidth, 10);
-        const slideGap = parseInt(this.options.slideGap, 10);
-        const scrollAmount = this.currentIndex * (slideWidth + slideGap);
-        this.slidesContainer.scrollTo({
-            left: scrollAmount,
-            behavior: 'smooth'
-        });
-    }
+	prev() {
+		if (this.currentIndex > 0) {
+			this.currentIndex--;
+			this.update();
+		}
+	}
+
+	goTo(index) {
+		if (index >= 0 && index < this.slides.length) {
+			this.currentIndex = index;
+			this.update();
+		}
+	}
+
+	update() {
+		const slideWidth = parseInt(this.options.slideWidth, 10);
+		const slideGap = parseInt(this.options.slideGap, 10);
+		const scrollAmount = this.currentIndex * (slideWidth + slideGap);
+
+		this.isProgrammaticScroll = true;
+		this.slidesContainer.scrollTo({
+			left: scrollAmount,
+			behavior: "smooth",
+		});
+
+		setTimeout(() => {
+			this.isProgrammaticScroll = false;
+		}, 300); // A bit longer than the scroll debounce
+	}
+
+	setIndexAfterScroll() {
+		const scrollLeft = this.slidesContainer.scrollLeft;
+		const slideWidth = parseInt(this.options.slideWidth, 10);
+		const slideGap = parseInt(this.options.slideGap, 10);
+		this.currentIndex = Math.round(scrollLeft / (slideWidth + slideGap));
+
+		this.update();
+	}
 }
